b62337f253748400968d03838fe1db3c6b08bc79
d3qnew <d3qnew@LTB>
2016-07-19 15:58:49 +0800

    bar

--- bld/Windows-x64/Debug/www/js/c/main.js
+++ bld/Windows-x64/Debug/www/js/c/main.js
@@ -21,19 +21,17 @@ $(document).ready(function () {
         $(".menubt").css("line-height", alineheight + "px");         //菜单栏按钮高度
         $(".menu_select").width(window.innerWidth);                 //菜单栏菜单高度
 
-
-
-        $(".menu_select").each(function () {
-
-            var count_li = $(this).children("li").length;
-            var top_range = wh - (count_li+1) * alineheight-3;     //距顶部距离 等于 窗口高度（减按钮栏）-(1+li菜单数量)*单行高度
-            $(this).css("top", top_range + "px");
-
-        });
-
         
 
-        
+        $(".menubt").each(function () {
+
+                var count_li = $(this).find("li").length;
+                var top_range = wh - (count_li + 1.2) * alineheight;      //距顶部距离 等于 窗口高度（减按钮栏）-(1+li菜单数量)*单行高度
+                $(this).children("ul").css("top", top_range + "px");
+
+            });
+            
+       
 
 
     }
@@ -47,16 +45,13 @@ $(document).ready(function () {
     //菜单跳转
 
     $(".menubt span").click(function () {
-
-        $(".menu_select").hide();        
-        $(this).siblings("ul").show();           
-
+        $(".menu_select").hide();
+        $(this).siblings("ul").show();
     });
 
     $(".menu_select li").click(function () {
         $(".menu_select").hide();
     });
-   
 
 
     $("#menubar_new").click(function () {
@@ -76,8 +71,6 @@ $(document).ready(function () {
     $("#menubar_setopt").click(function () {
         location.hash = "#setopt";
     });
-
-
 
 
 
